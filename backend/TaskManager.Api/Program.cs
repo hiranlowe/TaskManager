e2ae0206c6b05996207a12d70b504332aa01cc11
Hiran Lowe (99x) <hlowe@trintech.com>
2026-06-21 13:17:34 +0530

    Add https frontend

--- backend/TaskManager.Api/Program.cs
+++ backend/TaskManager.Api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyHeader()
               .AllowAnyMethod()
-              .WithOrigins("http://localhost:4200");
+              .WithOrigins("http://localhost:4200", "https://localhost:4200");
     });
 });
 
